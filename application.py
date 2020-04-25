a7afa87d1a9ac8681f919ce638b4224484b8ee89
Maximillian Guo <maximillian.guo@gmail.com>
2020-04-24 23:27:25 -0400

    Add Layouts

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request
 from datetime import datetime
 
 app = Flask(__name__)
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('landing.html')
+
+@app.route('/healthCare')
+def healthCare():
+    return render_template('healthCare.html')
